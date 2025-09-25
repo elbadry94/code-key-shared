d1f3dffa4af02b5560f46c06fecec70ef41155f0
Elbadry Mahmoud <elbadrym3@gmail.com>
2025-09-26 00:06:16 +0300

    change publish version , fix LoadingInterceptor issue

--- projects/code-key-shared-ui/src/lib/pages/interceptors/loading/loading.interceptor.ts
+++ projects/code-key-shared-ui/src/lib/pages/interceptors/loading/loading.interceptor.ts
@@ -10,13 +10,14 @@ export class LoadingInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     let apiurl = '';//  environment.HOST_API
-    // if (request.url.includes(environment.HOST_API)) {
+    //if (request.url.includes(environment.HOST_API)) {
     if (request.url.includes(apiurl)) {
       this.loadingService.setLoading(true, request.url);
 
-    //   return next.handle(request).pipe(finalize(() => this.loadingService.setLoading(false, request.url)));
-    // }
+      return next.handle(request).pipe(finalize(() => this.loadingService.setLoading(false, request.url)));
+    }
 
     return next.handle(request);
   }
 }
+
